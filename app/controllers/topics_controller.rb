8bcd908c918f61ef9683ee85321d91d2b763b9bf
Srikar Konudula <srikonudula@gmail.com>
2019-04-13 20:30:35 +0000

    Added a text box in the welcome page and added the create and show methods in topics_controller

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -1,2 +1,20 @@
 class TopicsController < ApplicationController
+    def new
+    end
+    def create
+        @topics = Topics.new(topics_params)
+        #byebug
+        
+         @topics.save
+        redirect_to @topics
+    end
+    
+    def show
+        @topics = Topics.find(params[:id])
+    end
 end
+
+private
+    def topics_params
+        params.require(:topics).permit(:name, :description)
+    end
