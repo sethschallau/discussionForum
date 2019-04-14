4cbf0eb9265d99dce5152860391921043497660a
Seth Schallau <sethmschallau@gmail.com>
2019-04-14 16:58:26 -0400

    created the topic admin page, added the ability to create new topics, and to show their pages.

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -1,2 +1,28 @@
 class TopicsController < ApplicationController
+    def new
+
+    end
+
+    def create
+        @topic = Topic.new(topic_params)
+
+        @topic.save
+        redirect_to @topic
+    end
+
+    def show
+        @topic = Topic.find(params[:id])
+    end
+
+    def index
+        @topics = Topic.all
+    end
+    
+
 end
+
+private
+    def topic_params
+        params.require(:topic).permit(:name, :description)
+    end
+
