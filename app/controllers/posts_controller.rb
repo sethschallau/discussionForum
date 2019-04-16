ed3e6289f68cd779ab233dbac12a12bfa1d54189
EC2 Default User <ec2-user@ip-172-31-39-148.us-east-2.compute.internal>
2019-04-16 14:47:31 +0000

    POSTS EXISTS

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,2 +1,16 @@
 class PostsController < ApplicationController
+    def new
+    
+    end
+    def create 
+        @topic = Topic.find(params[:topic_id])
+        @post = @topic.posts.create(post_params)
+        redirect_to @topic
+    end
+    private
+        def post_params
+            params.require(:post).permit(:title, :text)
+        end
 end
+
+
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   get 'welcome/index'
 
-  resources :topics
+  resources :topics do
+    resources :posts
+  end
   
   root 'welcome#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
